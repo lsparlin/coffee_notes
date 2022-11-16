0b7406c1c46a68ae511882a79d363cbd398c9b56
Lewis Sparlin <hello@lewismsparlin.com>
2022-11-16 13:40:21 -0600

    Remove unused import

--- app/components/section/section.styles.ts
+++ app/components/section/section.styles.ts
@@ -1,8 +1,6 @@
 import {TextStyle, ViewStyle} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-import {FONT_WEIGHT_BOLD, FONT_WEIGHT_MED} from 'styles/commonStyles';
-
 function lightOrDarkColor(isDark: boolean): TextStyle {
   return {color: isDark ? Colors.white : Colors.black};
 }
@@ -14,19 +12,19 @@ export const sectionContainerStyle: ViewStyle = {
 
 export const sectionTitleStyle = (isDarkMode: boolean): TextStyle => ({
   fontSize: 24,
-  fontWeight: FONT_WEIGHT_BOLD,
+  fontWeight: '600',
   ...lightOrDarkColor(isDarkMode),
 });
 
 export const sectionSubtitleStyle = (isDarkMode: boolean): TextStyle => ({
   fontSize: 20,
-  fontWeight: FONT_WEIGHT_MED,
+  fontWeight: '400',
   ...lightOrDarkColor(isDarkMode),
 });
 
 export const sectionDescriptionStyle = (isDarkMode: boolean): TextStyle => ({
   marginTop: 8,
   fontSize: 18,
-  fontWeight: FONT_WEIGHT_MED,
+  fontWeight: '400',
   ...lightOrDarkColor(isDarkMode),
 });
--- app/components/section/section.tsx
+++ app/components/section/section.tsx
@@ -1,6 +1,5 @@
 import React, {type PropsWithChildren} from 'react';
 import {Text, useColorScheme, View} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {
   sectionContainerStyle,
